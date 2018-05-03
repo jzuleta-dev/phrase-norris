a03e587265ce65b6235ca581335efd7f6136ad7f
Jose <jose.zuleta@governance.com>
2018-05-03 05:40:50 +0200

    Create testing and code improvement

--- src/redux/FavouriteRedux.js
+++ src/redux/FavouriteRedux.js
@@ -1,6 +1,12 @@
 import { createReducer, createActions } from 'reduxsauce'
 import Immutable from 'seamless-immutable'
-import { isNil, isEmpty, propEq, findIndex } from 'ramda'
+import { isNil, isEmpty, propEq, findIndex, has} from 'ramda'
+
+const hasId = has('id')
+const hasJoke = has('joke')
+const isValid = (phrase) => {
+  return hasId(phrase) && hasJoke(phrase)
+}
 
 const remove_item = (index, array) => {
   return array.slice(0, index).concat(array.slice(index + 1))
@@ -27,10 +33,17 @@ export const addPhrase = (state, {phrase}) => {
     return INITIAL_STATE
   }
 
-  return findIndex(propEq('id', phrase.id))(state.phrases) === -1 ? state.merge({phrases: state.phrases.concat([phrase])}) : state
+  if (isValid(phrase)) {
+    return findIndex(propEq('id', phrase.id))(state.phrases) === -1 ? state.merge({phrases: state.phrases.concat([phrase])}) : state
+  } else {
+    return state
+  }
 }
 
 export const removePhrase = (state, {id}) => {
+  if (isNil(state) || isEmpty(state)) {
+    return INITIAL_STATE
+  }
   const index = findIndex(propEq('id', id))(state.phrases)
   return index === -1 ? state : state.merge({phrases: remove_item(index, state.phrases)})
 }
---
+++ src/test/Favourites/favourites.test.js
@@ -0,0 +1,59 @@
+import * as J from 'jest'
+import FavouriteActions, {addPhrase as addPhraseReducer, removePhrase as removePhraseReducer} from '../../redux/FavouriteRedux'
+import Immutable from 'seamless-immutable'
+
+const INITIAL_STATE = Immutable({
+  phrases: []
+})
+
+const PARTIAL_STATE = Immutable({
+  phrases: [{
+    id: 243,
+    joke: "Contrary to popular belief, the Titanic didn't hit… he was doing the backstroke across the Atlantic."
+  }]
+})
+
+const phrase = {
+    id: 243,
+    joke: "Contrary to popular belief, the Titanic didn't hit… he was doing the backstroke across the Atlantic."
+  }
+
+const invalidPhrase = {
+    joke: "Contrary to popular belief, the Titanic didn't hit… he was doing the backstroke across the Atlantic."
+  }
+
+describe('Add Phrase Reducer', () => {
+  it('Should return initial state when undefined', () => {
+    expect(addPhraseReducer(undefined, {})).toEqual(INITIAL_STATE)
+  });
+  it('Should initial state when empty', () => {
+    expect(addPhraseReducer({}, {})).toEqual(INITIAL_STATE)
+  });
+  it('Should add a valid phrase', () => {
+    expect(addPhraseReducer(INITIAL_STATE, {phrase})).toEqual({
+      phrases: [{
+        id: 243,
+        joke: "Contrary to popular belief, the Titanic didn't hit… he was doing the backstroke across the Atlantic."
+      }]
+    })
+  });
+
+  it('Should add a valid phrase', () => {
+    expect(addPhraseReducer(INITIAL_STATE, {phrase: invalidPhrase})).toEqual(INITIAL_STATE)
+  });
+})
+
+describe('Remove Phrase Reducer', () => {
+  it('Should return initial state when undefined', () => {
+    expect(removePhraseReducer(undefined, {})).toEqual(INITIAL_STATE)
+  });
+  it('Should initial state when empty', () => {
+    expect(removePhraseReducer({}, {})).toEqual(INITIAL_STATE)
+  });
+  it('Should remove a valid id', () => {
+    expect(removePhraseReducer(PARTIAL_STATE, {id: 243})).toEqual(INITIAL_STATE)
+  });
+  it('Should not remove an non existing id', () => {
+    expect(removePhraseReducer(PARTIAL_STATE, {id: 23})).toEqual(PARTIAL_STATE)
+  })
+})
